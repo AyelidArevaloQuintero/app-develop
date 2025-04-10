2cda7c3b4949d5736e038815586db33f84820ed9
Ayelid <aarevaloq@ufpso.edu.co>
2025-04-09 20:34:51 -0500

    consumiendo api reqrest

--- js/loginService.js
+++ js/loginService.js
@@ -2,28 +2,7 @@ document.getElementById("formLogin").addEventListener('submit', function(e){
     e.preventDefault();
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    let message = "";
-    let alertType = "";
-
-    if(email === "" || password === ""){
-        alertType = "Warnig";
-        message('Por favor completa todos los campos');
-    }
-    else if(email === 'aarevaloq@ufpso.edu.co' && password === '12345'){
-        alertType = "Success";
-        message('Inicio de sesión exitoso');
-    }
-    else{
-        alertType = "Danger";
-        message('Correo o contraseña no validos');
-    }
-    let alert = `
-        <div class = "alert alert-${alertType} alert-dismissible fade show" role="alert">
-            ${message}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-    `;
-    document.getElementById('alert').innerHTML = alert;
+    login (email, password)
 })
 
 function login(email, password){
