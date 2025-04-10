0e9df0dc474cb20f3a6ba15c4dbc59e1d9ae2485
Ayelid <aarevaloq@ufpso.edu.co>
2025-04-09 20:12:41 -0500

    login con js datos quemados

--- js/loginService.js
+++ js/loginService.js
@@ -2,14 +2,26 @@ document.getElementById("formLogin").addEventListener('submit', function(e){
     e.preventDefault();
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
+    let message = ""
+    let alertType = ""
 
     if(email === "" || password === ""){
-        console.log('Por favor completa todos los campos');
+        alertType = "Warnig"
+        message('Por favor completa todos los campos');
     }
     else if(email === 'aarevaloq@ufpso.edu.co' && password === '12345'){
-        console.log('Inicio de sesión exitoso');
+        alertType = "Success"
+        message('Inicio de sesión exitoso');
     }
     else{
-        console.log('Correo o contraseña no validos');
+        alertType = "Danger"
+        message('Correo o contraseña no validos');
     }
+    let alert = `
+        <div class="alert alert-${alertType} alert-dismissible fade show" role="alert">
+            ${message}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+`;
+    document.getElementById('alert').innerHTML = alert;
 })
