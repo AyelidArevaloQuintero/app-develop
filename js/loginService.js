e10327c18218f4267d597a92598a4960b567687b
Ayelid07 <aarevaloq@ufpso.edu.co>
2025-05-21 07:44:19 -0500

    Clases atrasadas

--- js/loginService.js
+++ js/loginService.js
@@ -2,55 +2,55 @@ document.getElementById("formLogin").addEventListener('submit', function(e){
     e.preventDefault();
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    login (email, password)
+    
+    login(email, password)
 })
 
 function login(email, password){
-    let message = '';
-    let alertType = '';
+    let message = ''
+    let alertType = ''
     localStorage.removeItem('token')
     fetch("https://reqres.in/api/login", {
         method: "POST",
         headers: {
-            "Content-type": "aplication/json",
+            "Content-type": "application/json",
             'x-api-key': 'reqres-free-v1'
         },
         body: JSON.stringify({ email, password })
     })
-    .then((responce) => {
-        if(responce.status === 200){
-            alertType = "Success";
+    .then((response) =>{
+        if(response.status === 200){
+            alertType = 'success'
             message = 'Inicio de sesión exitoso';
-            console.log("Todo bien" + responce)
+            console.log('responde bien'+ response)
             alertBuilder(alertType, message)
-            localStorage.setItem('token', 'cute09cute07cute05cute13')
+            response.json().then((data) => {
+                localStorage.setItem('token', data.token)
+            })            
             setTimeout(() => {
                 location.href = 'admin/dashboard.html'
-            }, 2000) // 2000ms = 2 segundos
-            
+            }, 2000) // 2000 ms = 2 segundos
         }
         else{
-            alertType = "Danger";
-            message = 'Correo o contraseña no validos';
+            alertType = 'danger'
+            message = 'Correo o contraseña incorrectos.';
             alertBuilder(alertType, message)
-        }
+        }        
     })
-
     .catch((error) => {
-        alertType = "Danger";
-        message = 'Error inesperado';
+        alertType = 'danger'
+        message = 'Error inesperado.';
         console.error(error)
         alertBuilder(alertType, message)
     })
-    
 }
 
 function alertBuilder(alertType, message){
-    let alert = `
-    <div class="alert alert-${alertType} alert-dismissible fade show" role="alert">
-        ${message}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-`;
-document.getElementById('alert').innerHTML = alert;
+    const alert = `
+        <div class="alert alert-${alertType} alert-dismissible fade show" role="alert">
+            ${message}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    `;
+    document.getElementById('alert').innerHTML = alert;
 }
